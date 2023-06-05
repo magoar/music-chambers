42df6b5a4da736a812f5dd92ff9595bb1a38967e
Mago Arcera <magocali44@gmail.com>
2023-06-05 15:51:00 +0200

    created group model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_06_05_134823) do
+ActiveRecord::Schema[7.0].define(version: 2023_06_05_135031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -24,6 +24,14 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_134823) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_festivals_on_user_id"
+  end
+
+  create_table "groups", force: :cascade do |t|
+    t.string "name"
+    t.bigint "festival_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["festival_id"], name: "index_groups_on_festival_id"
   end
 
   create_table "musicians", force: :cascade do |t|
@@ -46,4 +54,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_06_05_134823) do
   end
 
   add_foreign_key "festivals", "users"
+  add_foreign_key "groups", "festivals"
 end
