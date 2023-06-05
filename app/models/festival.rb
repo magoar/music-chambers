9f334290c54d47091202110be20529d94f6850fb
Mago Arcera <magocali44@gmail.com>
2023-06-05 17:22:40 +0200

    added relations models

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,4 +1,6 @@
 class Festival < ApplicationRecord
   belongs_to :user
-  has_many :rooms, :groups, :rehearsals
+  has_many :rooms
+  has_many :groups
+  has_many :rehearsals
 end
--- app/models/requirement.rb
+++ app/models/requirement.rb
@@ -1,2 +1,4 @@
 class Requirement < ApplicationRecord
+  has_many :room_requirements
+  has_many :musician_requirements
 end
