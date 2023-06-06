d9c67e925c4f135055cd84a0c5305270656513ed
Jan Plewa <jan.plewa@web.de>
2023-06-06 15:53:00 +0200

    Seed file is done :) Also added some has_many stuff to groups and musicians

--- app/models/group.rb
+++ app/models/group.rb
@@ -1,5 +1,7 @@
 class Group < ApplicationRecord
   belongs_to :festival
   has_many :rehearsals
+  has_many :members
   has_many :musicians, through: :members
+  has_many :requirements, through: :musicians
 end
--- app/models/musician.rb
+++ app/models/musician.rb
@@ -1,4 +1,5 @@
 class Musician < ApplicationRecord
+  has_many :musician_requirements
   has_many :requirements, through: :musician_requirements
   has_many :groups, through: :members
 end
