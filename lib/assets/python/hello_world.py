1cdadf6b1c0ba11251f26149dc4f26dc9bfd0c5e
Jan Plewa <jan.plewa@web.de>
2023-06-08 15:27:30 +0200

    z3 is now running locally

--- lib/assets/python/hello_world.py
+++ lib/assets/python/hello_world.py
@@ -1,7 +1,15 @@
 import sys
+import z3
 
 input = sys.argv[1]
 
 print(input)
 
+from z3 import *
 
+x = Real('x')
+y = Real('y')
+s = Solver()
+s.add(x + y > 5, x > 1, y > 1)
+print(s.check())
+print(s.model())
