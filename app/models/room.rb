69dd2a0ca5d1d9978ae2532accd82733a3a80d9e
Mago Arcera <magocali44@gmail.com>
2023-06-12 15:12:25 +0200

    added uniqness of name only inside of a festival, for rooms, groups and musicians

--- app/models/room.rb
+++ app/models/room.rb
@@ -3,7 +3,9 @@ class Room < ApplicationRecord
   has_many :room_requirements, dependent: :destroy
   has_many :requirements, through: :room_requirements
   has_many :rehearsals, dependent: :destroy
-  validates :name, uniqueness: true, presence: true
+  # validations for name
+  validates :name, presence: true
+  validates_uniqueness_of :name, scope: :festival_id
   validates :size, presence: true, comparison: { greater_than: 0 }
   # scope for creating a custom query of the database. Show new created items on top.
   scope :ordered, -> { order(created_at: :desc) }
