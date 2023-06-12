218da9f537e5450004c17b288152c9135b337b8c
Jan Plewa <jan.plewa@web.de>
2023-06-12 12:51:19 +0200

    Since I moved everything from the rehearsals controller to the schedules controller, I removed the associate files. Also I removed old python files that I used for testing

--- app/controllers/rehearsals_controller.rb
+++
@@ -1,2 +0,0 @@
-class RehearsalsController < ApplicationController
-end
--- test/controllers/rehearsals_controller_test.rb
+++
@@ -1,7 +0,0 @@
-require "test_helper"
-
-class RehearsalsControllerTest < ActionDispatch::IntegrationTest
-  # test "the truth" do
-  #   assert true
-  # end
-end
